57de8c0e14c6aa8c9b1e052536fee4d84599917c
unsky559 <6kostya@gmail.com>
2022-08-31 02:05:13 +0300

    fix: title change

--- src/requests/getTwoEven.ts
+++ src/requests/getTwoEven.ts
@@ -2,8 +2,8 @@ import getRandom from './getRandom';
 
 /*
 * Imitates image loading process.
-* Returns true if both of random numbers from `getRandom()` function are doubles
-* Making another request recursively while two numbers is doubles
+* Returns true if both of random numbers from `getRandom()` function are even
+* Making another request recursively while two numbers is not even
 * */
 export default async function getTwoEven(): Promise<boolean> {
   const results = await Promise.all([getRandom(), getRandom()]);
