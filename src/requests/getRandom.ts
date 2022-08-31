7885631b2c009d6edccdebfae27c9bf81e48f100
unsky559 <6kostya@gmail.com>
2022-08-31 13:21:21 +0300

    refactor: from HOC to custom hook

--- src/requests/getRandom.ts
+++
@@ -1,5 +0,0 @@
-export default async function getRandom() {
-  const result = await fetch('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new');
-  const data = await result.text();
-  return Number(data);
-}
